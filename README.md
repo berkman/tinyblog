4463af3d67ee48ecf5e3ac696333f2ec79e96314
Mike Berkman <mberkman@cisco.com>
2016-01-30 15:23:18 -0500

    tweaking api

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 tinyblog
 
 A tiny blog written in Python leveraging Django/Bootstrap/Postgres and built to run on Heroku
+
+
+
+
+
+
+cli examples:
+    from main.models import Post, Comment
+    from django.utils import timezone
+    from django.contrib.auth.models import User
+
+    u = User.objects.get(pk=1)
+    p = Post.objects.get(pk=1)
+
+    p.comment_set.create(comment='you suck!', user=u, comment_pub=timezone.now())
--- main/models.py
+++ main/models.py
@@ -5,4 +5,16 @@ class Post(models.Model):
     subject = models.CharField(max_length=200)
     body = models.CharField(max_length=2000)
     user = models.ForeignKey(User)
-    post_timestamp = models.DateTimeField('date published')
+    post_pub = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.subject
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=2000)
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_pub = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.comment

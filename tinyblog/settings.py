27bdc61112d3985d46d075140a6315aba61ca9c4
Mike Berkman <mberkman@cisco.com>
2016-01-30 12:50:03 -0500

    Remove debugging for Heroku

--- tinyblog/settings.py
+++ tinyblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 
 # Application definition

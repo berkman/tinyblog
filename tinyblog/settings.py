edf065729fa46b8184d5f0a3ae5b187336ef03c2
Mike Berkman <mike@berkman.co>
2016-02-03 19:37:12 -0500

    Fixed the Error 500 by modifying the DB and DEBUG vars

--- tinyblog/settings.py
+++ tinyblog/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', False)
+DEBUG = True
+#os.environ.get('DJANGO_DEBUG', False)
 
 # Set environment name for testing purposes
-APP_ENV = os.environ.get('APP_ENV', 'localhost')
+#APP_ENV = os.environ.get('APP_ENV', 'localhost')
 
 # Application definition
 
